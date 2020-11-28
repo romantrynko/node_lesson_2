6a59d2b36c0af204681b0cba7b2de697b80d96b8
romantrynko <trynkoroman25@gmail.comnode>
2020-11-28 19:45:29 +0200

    ....

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-const { users } = require('./users');
-
 const express = require('express');
 const exprsHbs = require('express-handlebars');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(process.cwd(), 'views')));
 
 app.set('view engine', '.hbs');
+
 app.engine('.hbs', exprsHbs({
     defaultLayout: false
 }));
@@ -19,48 +18,79 @@ app.engine('.hbs', exprsHbs({
 app.set('views', path.join(process.cwd(), 'views'));
 
 
+const usersPath = path.join(process.cwd(), 'users.json');
 
 app.get('/', (req, res) => {
     res.render('main');
-});
-
-app.get('/users', (req, res) => {
-    res.render('users', { users: users });
 });
 
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/users', (req, res) => {
+    fs.readFile(usersPath, (err, data) => {
+        const users = JSON.parse(data.toString())
+        res.render('users', { users: users });
+    })
+});
+
 app.get('/error', (req, res) => {
     res.render('errorPage');
 });
 
+
 app.post('/register', (req, res) => {
-    const { name, age, email } = req.body;
-    users.map(el => {
-        if (el.email !== email) {
-            users.push(name, age, email);
-            res.redirect('/users');
-            brake;
+    const { email, password } = req.body;
+
+    fs.readFile(usersPath, (err, data) => {
+        if (err) throw err;
+
+        const users = JSON.parse(data.toString());
+        const registeredUser = users.find(user => {
+            user.email === email && user.password === password;
+        });
+
+        if (!registeredUser) {
+            users.push(req.body);
+            fs.writeFile(usersPath, JSON.stringify(users));
+            res.redirect('/login');
         }
         res.redirect('/login');
-    })
+
+
+    });
+
+
 });
 
 app.post('/login', (req, res) => {
-    const { email } = req.body;
-    users.find(el => {
-        if (el.email.toLocaleLowerCase() === email.toLocaleLowerCase()) {
-            res.redirect('/users');
+    const { email, password } = req.body;
+
+    fs.readFile(usersPath, (err, data) => {
+        if (err) throw err;
+
+        const users = JSON.parse(data.toString());
+        const emailTrue = users.find(user => (user.email === email && user.password === password));
+        if (!emailTrue) {
+            res.redirect('/error');
         }
-        res.redirect('/error');
+        res.redirect('/users');
     })
+
+
 });
 
 app.get('/logout', (req, res) => {
-    users.pop();
-    res.redirect('/')
+    fs.readFile(usersPath, (err, data) => {
+        const users = JSON.parse(data.toString())
+        users.pop();
+    });
+    res.render('main');
 });
 
 app.listen(3000, () => {
--- users.js
+++
@@ -1,9 +0,0 @@
-let usersArray = [
-    { name: 'Ivan', age: 25, email: "ivan@net" },
-    { name: 'Vova', age: 35, email: "vova@net" },
-    { name: 'Roma', age: 20, email: "roma@net" },
-]
-
-module.exports = {
-    users: usersArray
-}
