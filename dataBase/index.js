fc1c0e1f672bb0c3fec0d2f97f32dae691df3e5e
romantrynko <trynkoroman25@gmail.comnode>
2020-12-04 22:12:56 +0200

    sequelize

--- dataBase/index.js
+++ dataBase/index.js
@@ -1,10 +1,43 @@
-const mysql2 = require('mysql2');
+/* eslint-disable import/no-dynamic-require */
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
 
-const connection = mysql2.createConnection({
-    user: 'root',
-    password: 'root',
-    database: 'usersdb',
-    host: 'localhost'
-});
+module.exports = (() => {
+    let instance;
 
-module.exports = connection.promise();
+    const initConnection = () => {
+        const client = new Sequelize('usersdb', 'host', 'host', {
+            host: 'localhost',
+            dialect: 'mysql'
+        });
+
+        const models = {};
+        const modelsPath = path.join(process.cwd(), 'dataBase', 'models');
+
+        const getModels = () => {
+            fs.readdir(modelsPath, (err, files) => {
+                files.forEach((file) => {
+                    const [model] = file.split('.');
+                    models[model] = (require(path.join(modelsPath, model)))(client, DataTypes);
+                });
+            });
+        };
+
+        return {
+            setModels: () => getModels(),
+            getModel: (modelName) => models[modelName]
+        };
+    };
+
+    return {
+        getInstance: () => {
+            if (!instance) {
+                // create connection
+                instance = initConnection();
+            }
+
+            return instance;
+        }
+    };
+})();
