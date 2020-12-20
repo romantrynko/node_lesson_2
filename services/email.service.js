5beed8a182eeb5eb72fc794deb67f8ee1a3215cf
romantrynko <trynkoroman25@gmail.com>
2020-12-20 17:08:05 +0200

    email_service

---
+++ services/email.service.js
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
+const mailer = require('nodemailer');
+
+const transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+        pass: 'password',
+        user: 'romantrynko@gmail.com'
+    }
+});
+
+class EmailService {
+    sendMail(userMail) {
+        try {
+            return transporter.sendMail({
+                from: 'Theme',
+                to: userMail,
+                subject: 'subject',
+                html: ''
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+module.exports = new EmailService();
